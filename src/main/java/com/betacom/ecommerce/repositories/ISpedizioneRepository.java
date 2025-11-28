8b79a6c13e05bf77583d2a4654dd3f7a1e4b5407
Gérard Smal <g.smal@betacom.it>
2025-11-28 15:08:02 +0100

    Repository for Spedizione

---
+++ src/main/java/com/betacom/ecommerce/repositories/ISpedizioneRepository.java
@@ -0,0 +1,9 @@
+package com.betacom.ecommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.betacom.ecommerce.models.Spedizione;
+
+public interface ISpedizioneRepository extends JpaRepository<Spedizione, Integer>{
+
+}
